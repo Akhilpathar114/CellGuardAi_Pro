5f01c110e155a0f7df229d1cba578697b7423762
Akhil Pathar <akhilmpathar@gmail.com>
2025-12-10 19:55:10 +0530

    Add files via upload

--- cellguardai/explainability.py
+++ cellguardai/explainability.py
@@ -1,1 +1,6 @@
 
+def explain_row(row):
+    msgs=[]
+    if row.get("Temp1",0)>60: msgs.append("High temperature")
+    if row.get("C_Diff",0)>80: msgs.append("Cell imbalance")
+    return {"messages":msgs}
