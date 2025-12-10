f1b0d64b33f7056e3261076ade11c9e90a5dbb0b
Akhil Pathar <akhilmpathar@gmail.com>
2025-12-10 19:54:07 +0530

    Add files via upload

--- cellguardai/ai_model.py
+++ cellguardai/ai_model.py
@@ -1,1 +1,8 @@
 
+import numpy as np
+def compute_risk_scores(df):
+    df = df.copy()
+    df["risk_score"] = np.clip(
+        (df.get("Temp1",0)/80 + df.get("C_Diff",0)/100)/2,0,1
+    )
+    return df
